ce775f9fce1123403b5a11f137b5e23b9da402cf
GoodWeather0322 <a82521112@gmail.com>
2024-09-29 11:45:08 +0800

    ✨ feat: 增加驗證token expire time

--- app/api/deps.py
+++ app/api/deps.py
@@ -3,10 +3,13 @@ from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
 from sqlalchemy.orm import Session
+from datetime import datetime, timezone
 from app import models, schemas
 from app.db.session import SessionLocal
 from app.core.config import settings
 from app.utils.security import verify_password
+from app.core.logging_config import logging
+logger = logging.getLogger(__name__)
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login/access-token")
 
@@ -31,13 +34,23 @@ async def get_current_user(
         detail="無法驗證憑證",
         headers={"WWW-Authenticate": "Bearer"},
     )
+    expired_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="憑證已過期",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
     try:
         payload = jwt.decode(
             token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
         )
-        username: str = payload.get("sub")
+        username = payload.get("sub")
         if username is None:
             raise credentials_exception
+        exp = payload.get("exp")
+        if exp is None:
+            raise credentials_exception
+        if datetime.now(timezone.utc) > datetime.fromtimestamp(exp, timezone.utc):
+            raise expired_exception
         token_data = schemas.TokenData(username=username)
     except JWTError:
         raise credentials_exception
