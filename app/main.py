95ffcfde55335f77f814a4cd26480a0c44d423b9
CHRIS.CHIEN 簡靖岳 <CHRIS.CHIEN@deltaww.com>
2024-09-30 14:57:42 +0800

    📝 docs: write down TODO in main.py

--- app/main.py
+++ app/main.py
@@ -19,3 +19,6 @@ app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 # 應用啟動時創建 admin user
 create_admin_user()
+
+# TODO: 牽涉到儲存檔名可能需要改成UUID儲存，避免使用者一直用重複檔名導致儲存的影像一直被複寫
+# TODO: 可能需要資料庫對應儲存的UUID檔名與使用者上傳的檔名
