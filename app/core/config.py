fbd4bef321cb170e6489a74dc93d0ec8b0e97879
CHRIS.CHIEN 簡靖岳 <CHRIS.CHIEN@deltaww.com>
2024-10-01 09:37:02 +0800

    🐛 fix: 加強SECRET_KEY安全性驗證

--- app/core/config.py
+++ app/core/config.py
@@ -1,6 +1,7 @@
 # app/core/config.py
 import secrets
 from pydantic_settings import BaseSettings
+from pydantic import field_validator
 
 
 class Settings(BaseSettings):
@@ -10,9 +11,20 @@ class Settings(BaseSettings):
     MYSQL_DATABASE: str
     MYSQL_USER: str
     MYSQL_PASSWORD: str
-    SECRET_KEY: str = secrets.token_urlsafe(32)
+    SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+
+    @field_validator("SECRET_KEY")
+    def validate_secret_key(cls, v):
+        if not v or len(v) < 32:  # 檢查 SECRET_KEY 是否存在且長度是否小於32
+            user_input = (
+                v if v else secrets.token_urlsafe(16)
+            )  # 使用者輸入或生成16字符長度的隨機URL安全密鑰
+            return user_input + secrets.token_urlsafe(
+                16
+            )  # 融合使用者輸入和新的16字符長度的隨機URL安全密鑰
+        return v
 
     class Config:
         env_file = ".env"
